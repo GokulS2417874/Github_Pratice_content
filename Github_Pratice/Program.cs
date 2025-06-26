f925dbeb443f477a70ff32bbfcaac33028d4ae93
2417874 <2417874@LTIN605354>
2025-06-26 15:06:00 +0530

    Add project files.

---
+++ Github_Pratice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_Pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
